54494326f6ffbb83a85259b9ab077315aec38114
Orlando Aparicio <orlando.nabil@gmail.com>
2024-03-17 13:43:32 -0500

    Refactor CSS styles and update component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import { useSettings } from "./utils/hooks/use-settings";
-import viteLogo from "/vite.svg";
 import useSetFonts from "./utils/hooks/use-set-fonts";
 
 function App() {
-  const settings = useSettings();
   useSetFonts();
 
   return (
-    <div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="mainComponent">
+        <h1>pomodoro</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button>count is {settings?.settings.fonts}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </main>
   );
 }
 
