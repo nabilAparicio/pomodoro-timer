b598873f76117ecabc8e93609461b26c9ffe68de
Orlando Aparicio <orlando.nabil@gmail.com>
2024-04-01 19:41:31 -0500

    Add ClockDisplay component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { ClockDisplay } from "./components/clock-display/clock-display";
 import ThreeWaySwitch from "./components/three-way-switch/three-way-switch";
 import useSetFonts from "./utils/hooks/use-set-fonts";
 
@@ -10,6 +11,9 @@ function App() {
       <div className="mainComponent">
         <h1>pomodoro</h1>
         <ThreeWaySwitch />
+        <ClockDisplay percentage={90} radius={150}>
+          <h2>25:00</h2>
+        </ClockDisplay>
       </div>
     </main>
   );
